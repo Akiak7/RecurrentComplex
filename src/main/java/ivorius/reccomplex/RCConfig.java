4a9737cbcade5ac444389c850b2cc8539012853a
Akiak7 <50582725+Akiak7@users.noreply.github.com>
2025-11-04 01:13:54 +0000

    Clear universal transformer presets before reload

--- src/main/java/ivorius/reccomplex/RCConfig.java
+++ src/main/java/ivorius/reccomplex/RCConfig.java
@@ -177,7 +177,13 @@ public class RCConfig
             mazeTimeout = config.getInt("mazeTimeout", CATEGORY_BALANCING, 20000, -1, 600000, "Maze generation timeout, in milliseconds. After the time is over, the maze generation will just give up.");
 
             universalTransformer = null;
+            universalTransformerPresets.clear();
             Collections.addAll(universalTransformerPresets, config.getStringList("universalTransformerPresets", CATEGORY_BALANCING, new String[0], "Transformer preset names that are gonna be applied to every single generating structure. Use this if you need to enforce specific rules (e.g. \"don't ever spawn wood blocks\" (with a replace transformer)."));
+
+            if (RecurrentComplex.logger != null && RecurrentComplex.logger.isDebugEnabled())
+            {
+                RecurrentComplex.logger.debug("Loaded {} universal transformer presets", universalTransformerPresets.size());
+            }
 
             globalToggles.clear();
             ConfigUtil.parseMap(config.getStringList("globalToggles", CATEGORY_BALANCING, new String[]{"treeLeavesDecay: true"}, "Global toggles that can be used in expressions. You can also add your own. Ex: 'treeLeavesDecay: true'."),
